5b5459c72fe64d8c01cfd938e86b17caa29cd44e
willy <willymwaid92@gmail.com>
2019-12-07 10:56:12 +0300

    Updates

---
+++ precache-manifest.19e60bad32a2310c76e4b4e7d52eb204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a268abc58dab9354ffb4ac3faf9f4da",
+    "url": "/loanbook/index.html"
+  },
+  {
+    "revision": "4cbc309eba590808dd3f",
+    "url": "/loanbook/static/css/main.0e4b8801.chunk.css"
+  },
+  {
+    "revision": "953684322014f182eb30",
+    "url": "/loanbook/static/js/2.d45c5586.chunk.js"
+  },
+  {
+    "revision": "4cbc309eba590808dd3f",
+    "url": "/loanbook/static/js/main.78a760d8.chunk.js"
+  },
+  {
+    "revision": "5e9b1b10cd1c39eb2457",
+    "url": "/loanbook/static/js/runtime-main.21c09d2e.js"
+  },
+  {
+    "revision": "22ebb32accef92bc2ff09d7e4fab7b47",
+    "url": "/loanbook/static/media/background.22ebb32a.jpg"
+  }
+]);
