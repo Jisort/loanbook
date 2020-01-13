560e6a743e8e1808f38a38f8be5e7c2f91957be9
willy <willymwaid92@gmail.com>
2020-01-13 16:01:42 +0300

    update -- build with electron

--- src/functions/baseUrls.js
+++ src/functions/baseUrls.js
@@ -1,10 +1,9 @@
 module.exports = {
     serverBaseUrl: function () {
         if(process.env.NODE_ENV === 'development') {
-            return 'https://c6d4a604.ngrok.io';
-            // return 'http://localhost:8000';
+            return 'http://localhost:8000';
         } else {
-            return 'https://c6d4a604.ngrok.io';
+            return 'https://my.jisort.com';
         }
     }
 };
